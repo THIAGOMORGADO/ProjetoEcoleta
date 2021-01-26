04c8d5be2b5c3fe8997563dc64fa7f567a4eccb0
thiago morgado <thiagomorgado81@gmail.com>
2021-01-26 09:26:39 -0300

    Projeto Ecoleta Finalizado

--- server/src/controllers/pointControllers.ts
+++ server/src/controllers/pointControllers.ts
@@ -1,15 +1,15 @@
-import {Request, Response} from 'express';
+import { Request, Response } from 'express';
 import knex from '../database/connection';
 
 class PointsController {
-  async index (req:Request, res:Response){
-     const {city, uf, items } = req.query;
+  async index(request: Request, response: Response) {
+    const { city, uf, items } = request.query;
 
     const parsedItems = String(items)
-    .split(',')
-    .map(item => Number(item.trim()));
+      .split(',')
+      .map(item => Number(item.trim()));
 
-    const point = await knex('points')
+    const points = await knex('points')
       .join('point_items', 'points.id', '=', 'point_items.point_id')
       .whereIn('point_items.item_id', parsedItems)
       .where('city', String(city))
@@ -17,38 +17,39 @@ class PointsController {
       .distinct()
       .select('points.*');
 
-      const serializedPoints = point.map(point => {
-        return {
-          ...point,
-          image_url: `http://192.168.1.12:3333/uploads/${point.image}`, 
-        };
-      })
-    
-      return res.json(serializedPoints)
+    const serializedPoints = points.map(point => {
+      return {
+        ...point,
+        image_url: `http://192.168.1.69:3333/uploads/${point.image}`,
+      };
+    });
+
+    return response.json(serializedPoints);
   }
-  
-  async show (req: Request, res: Response) {
-    const {id} = req.params;
+
+  async show(request: Request, response: Response) {
+    const { id } = request.params;
 
     const point = await knex('points').where('id', id).first();
 
-    if(!point){
-      return res.status(400).json({messager: 'ponto nao encontrado'})
+    if (!point) {
+      return response.status(400).json({ message: 'Point not found' });
     }
-    const serializedPoint = { 
-        ...point,
-        image_url: `http://192.168.1.12:3333/uploads/${point.image}`, 
-    }
+
+    const serializedPoint = {
+      ...point,
+      image_url: `http://192.168.1.69:3333/uploads/${point.image}`,
+    };
 
     const items = await knex('items')
-    .join('point_items', 'item_id', '=', 'point_items.item_id')
-    .where('point_items.point_id', id)
-    .select('items.title')
+      .join('point_items', 'items.id', '=', 'point_items.item_id')
+      .where('point_items.point_id', id)
+      .select('items.title');
 
-    return res.json({point: serializedPoint, items});
-
+    return response.json({ serializedPoint, items });
   }
-  async create (req: Request, res: Response)  {
+
+  async create(request: Request, response: Response) {
     const {
       name,
       email,
@@ -57,12 +58,13 @@ class PointsController {
       longitude,
       city,
       uf,
-      items
-    } = req.body;
-    
+      items,
+    } = request.body;
+
+    const trx = await knex.transaction();
 
     const point = {
-      image: req.file.filename,
+      image: request.file.filename,
       name,
       email,
       whatsapp,
@@ -70,31 +72,30 @@ class PointsController {
       longitude,
       city,
       uf,
-    }
-    const trx = await knex.transaction();
-    const insertedIds  = await trx('points').insert(point);
-  
+    };
+
+    const insertedIds = await trx('points').insert(point);
+
     const point_id = insertedIds[0];
-  
+
     const pointItems = items
-    .split(',')
-    .map((item: string) => Number(item.trim()))
-    .map((item_id: number) => {
-      return {
-        item_id,
-        point_id,
-      };
-    });
-  
+      .split(',')
+      .map((item: string) => Number(item.trim()))
+      .map((item_id: number) => {
+        return {
+          item_id,
+          point_id,
+        };
+      });
+
     await trx('point_items').insert(pointItems);
-    
+
     await trx.commit();
 
-    return res.json({
+    return response.json({
       id: point_id,
       ...point,
-
-    })
+    });
   }
 }
 
--- web/src/pages/CreatePoints/index.tsx
+++ web/src/pages/CreatePoints/index.tsx
@@ -2,7 +2,7 @@ import React, {useEffect, useState, ChangeEvent, FormEvent} from 'react';
 import {Link, useHistory}  from 'react-router-dom'
 import {FiArrowLeft} from 'react-icons/fi'
 import {Map, TileLayer, Marker,} from 'react-leaflet'
-
+import Dropzone from '../../components/dropzone'
 import {LeafletMouseEvent} from 'leaflet';
 import axios from 'axios';
 import api from '../../services/api';
@@ -38,6 +38,7 @@ const CreatePoints  = () => {
   const [selectedUf, setSelectedUf] = useState('0');
   const [selectedCity, setSelectedCity] = useState('0')
   const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0,0]);
+  const [selectedFile, setSelectedFile] = useState<File>();
 
   const history = useHistory();
   useEffect(() => {
@@ -108,21 +109,28 @@ const CreatePoints  = () => {
     const city = selectedCity;
     const [latitude, longitude] = selectedPosition;
     const items = selectedItems;
-    const data = {
-      name,
-      email,
-      whatsapp,
-      uf,
-      city,
-      latitude,
-      longitude,
-      items,
-    }
+
+    const data = new FormData();
+
+      data.append('name', name);
+      data.append('email', email);
+      data.append('whatsapp', whatsapp);
+      data.append('uf', uf);
+      data.append('city', city);
+      data.append('latitude', String(latitude));
+      data.append('longitude', String(longitude));
+      data.append('items', items.join(','));
+      
+      if(selectedFile){
+        data.append('image', selectedFile);
+      }
+    
     await api.post('points', data)
     alert('Ponto de coleta criado')
     history.push('/')
     
   }
+ 
   return(
     <div id="page-create-point">
       <header>
@@ -134,6 +142,7 @@ const CreatePoints  = () => {
       </header>
       <form onSubmit={handleSubmit}>
         <h1>Cadastro do <br/>  ponto de coleta</h1>
+          <Dropzone onFileUoloaded={setSelectedFile}/>
         <fieldset>
           <legend>
             <h2>Dados</h2>
